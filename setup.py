d58509680ee6d2d4adcb83fb6baad9fbee08e603
Philipp Schlegel <observing@web.de>
2021-10-01 15:31:25 +0100

    modify setup

--- setup.py
+++ setup.py
@@ -1,12 +1,23 @@
+import platform
+
 from setuptools import Extension, setup
 from Cython.Build import build_ext
+
+
+if platform.system() == "Windows":
+    compile_args = ["/openmp"]
+elif platform.system() == "Darwin":
+    compile_args = ["-Xpreprocessor", "-fopenmp", "-std=c++11"]
+else:
+    compile_args = ["-fopenmp", "-std=c++11"]
+
 
 ext_modules = [
     Extension(
         "fastcore._vertex_cython",
         ["fastcore/_vertex_cython.pyx"],
-        extra_compile_args=['-fopenmp'],
-        extra_link_args=['-fopenmp'],
+        extra_compile_args=compile_args,
+        extra_link_args=compile_args,
     )
 ]
 

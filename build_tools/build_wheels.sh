e1140a6dc3c99d0f4fb9312a88e0c979df07c328
Philipp Schlegel <observing@web.de>
2021-10-01 15:25:30 +0100

    CI: try yet another build setup

--- build_tools/build_wheels.sh
+++ build_tools/build_wheels.sh
@@ -5,27 +5,14 @@ set -x
 
 # OpenMP is not present on macOS by default
 if [[ "$RUNNER_OS" == "macOS" ]]; then
-    # Make sure to use a libomp version binary compatible with the oldest
-    # supported version of the macos SDK as libomp will be vendored into the
-    # scikit-learn wheels for macos. The list of binaries are in
-    # https://packages.macports.org/libomp/.  Currently, the oldest
-    # supported macos version is: High Sierra / 10.13. When upgrading this, be
-    # sure to update the MACOSX_DEPLOYMENT_TARGET environment variable in
-    # wheels.yml accordingly. Note that Darwin_17 == High Sierra / 10.13.
-    wget https://packages.macports.org/libomp/libomp-12.0.0_0+universal.darwin_17.i386-x86_64.tbz2 -O libomp.tbz2
-    sudo tar -C / -xvjf libomp.tbz2 opt
-
+    brew install libomp
     export CC=/usr/bin/clang
     export CXX=/usr/bin/clang++
-    export CPPFLAGS="$CPPFLAGS -Xpreprocessor -fopenmp -lomp"
-    export CFLAGS="$CFLAGS -I/opt/local/include/libomp"
-    export CXXFLAGS="$CXXFLAGS -I/opt/local/include/libomp"
-    export LDFLAGS="$LDFLAGS -Wl,-rpath,/opt/local/lib/libomp -L/opt/local/lib/libomp -lomp"
+    export CPPFLAGS="$CPPFLAGS -Xpreprocessor -fopenmp"
+    export CFLAGS="$CFLAGS -I/usr/local/opt/libomp/include"
+    export CXXFLAGS="$CXXFLAGS -I/usr/local/opt/libomp/include"
+    export LDFLAGS="$LDFLAGS -Wl,-rpath,/usr/local/opt/libomp/lib -L/usr/local/opt/libomp/lib -lomp"
 fi
 
-# The version of the built dependencies are specified
-# in the pyproject.toml file, while the tests are run
-# against the most recent version of the dependencies
-
-python -m pip install cibuildwheel
-python -m cibuildwheel --output-dir dist
+python -m pip install cibuildwheel==2.1.1
+python -m cibuildwheel --output-dir wheelhouse

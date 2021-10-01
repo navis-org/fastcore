128e6601b999126befbacd8bfe2a3f79d1c26d8e
Philipp Schlegel <observing@web.de>
2021-10-01 14:43:03 +0100

    CI builds: try installingg libomp too and set extra flags

--- build_tools/preflight.sh
+++ build_tools/preflight.sh
@@ -5,8 +5,8 @@ set -x
 
 # OpenMP is not present on macOS by default
 if [[ "$RUNNER_OS" == "macOS" ]]; then
-    brew install llvm
+    brew install llvm libomp
     export PATH="/usr/local/opt/llvm/bin:$PATH"
     export LDFLAGS="-L/usr/local/opt/llvm/lib -Wl,-rpath,/usr/local/opt/llvm/lib"
-    export CPPFLAGS="-I/usr/local/opt/llvm/include"
+    export CPPFLAGS="$CPPFLAGS -I/usr/local/opt/llvm/include -Xpreprocessor"
 fi
